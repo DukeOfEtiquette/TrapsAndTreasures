9ac7dd7565c44568d977e4888353c2e9c63a6d58
Bradley Cook <bradleycook@Bradleys-MacBook-Pro.local>
2017-04-22 13:47:50 -0700

    added player and gesture

--- TrapsAndTreasures/GameplayBController.swift
+++ TrapsAndTreasures/GameplayBController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+import SpriteKit
+
 
 
 class GameplayBController: UIViewController {
-    var tile_1: UIImage
+    
+    @IBOutlet weak var tile_1: UIImageView!
+    @IBOutlet weak var tile_2: UIImageView!
+    @IBOutlet weak var tile_3: UIImageView!
+    @IBOutlet weak var tile_4: UIImageView!
+    @IBAction func tap(_ sender: UITapGestureRecognizer) {
+    }
+    @IBOutlet weak var player: UIImageView!
+    
+  
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
     override func didReceiveMemoryWarning() {
