a5396aae67f30ab7bee540a13dbdd43c7ea908ff
Adam DuQuette <duquetteadam@gmail.com>
2017-04-26 22:22:48 -0700

    Implemented Tile inits and skeleton code for it's methods

--- TrapsAndTreasures/Tile.swift
+++ TrapsAndTreasures/Tile.swift
@@ -7,5 +7,31 @@
 //
 
 class Tile{
+    public var treasure: Treasure?
+    public var trap: MovementTrap?
     
+    
+    init(){
+        self.treasure = nil
+        self.trap = nil
+    }
+    
+    //TODO(Adam): Pretty sure we can make a hierarchy of inits to allow
+    //            any combination of traps/treasures to init
+    init(treasure: Treasure, trap: MovementTrap){
+        self.treasure = treasure
+        self.trap = trap
+    }
+    
+    
+    public func triggerTrap() -> Int{
+        //TODO(Adam): Define this function
+        return 0
+    }
+    
+    public func giveTreasure() -> MovementTrap{
+        //TODO(Adam): Define this function
+        
+        return MovementTrap()
+    }
 }
