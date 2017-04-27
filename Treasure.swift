0723ca10d9b769621dda2c0fb6ab631deef47931
Adam DuQuette <duquetteadam@gmail.com>
2017-04-26 21:42:56 -0700

    implemented Treasure according to draft model doc

--- Treasure.swift
+++ Treasure.swift
@@ -6,6 +6,12 @@
 //  Copyright © 2017 Dreamy Team. All rights reserved.
 //
 
+import Foundation
+
 class Treasure{
+    public let numTraps: Int
     
+    init(){
+        self.numTraps = Int(arc4random_uniform(UInt32(3)))
+    }
 }
