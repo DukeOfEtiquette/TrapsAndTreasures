622afba01a88bca28829eb8786d092adf7f350dd
Adam DuQuette <duquetteadam@gmail.com>
2017-04-28 15:38:54 -0700

    added some computed values to GameWorld'

--- TrapsAndTreasures/GameWorld.swift
+++ TrapsAndTreasures/GameWorld.swift
@@ -7,29 +7,39 @@
 //
 
 class GameWorld{
-    private var playerLocation: Tile
+    private var playerLocation: Int
     private var gameTiles: [Tile]
     
     init(){
-        self.playerLocation = Tile()
+        self.playerLocation = -1
         self.gameTiles = []
     }
     
-    init(playerLocation: Tile, gameTiles: [Tile]){
+    init(playerLocation: Int, gameTiles: [Tile]){
         self.playerLocation = playerLocation
         self.gameTiles = gameTiles
     }
-    
-    public func getPlayerLocation() -> Tile{
+
+    //Get playerLocation
+    var playerLocation: Int{
         return self.playerLocation
+    }
+
+    //Accept a tile location and set the playerLocation to it
+    public func setPlayerTile(tile: Int){
+        self.playerLocation = tile
     }
     
     public func movePlayer(){
         //TODO(Adam): Define this function
     }
     
-    public func isPlayer(onThisTile testTile: Tile) -> Bool{
-        //TODO(Adam): Define this function
-        return true
+    //Compare the player's current location against a tile location passed to func
+    public func isPlayer(onThisTile testTile: Int) -> Bool{
+        if(testTile == self.playerLocation){
+            return true
+        }else{
+            return false
+        }
     }
 }
