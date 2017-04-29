c71575385d9d05770b7421fb5922d0e7cc3a3ff8
Brad Cook <cookbrad@oregonstate.edu>
2017-04-28 17:39:30 -0700

    fixed the movement issues

--- TrapsAndTreasures/GameplayController.swift
+++ TrapsAndTreasures/GameplayController.swift
@@ -16,6 +16,7 @@ class GameplayController: UIViewController {
     var i = 0
     var movement = 46
     var traps = 2
+    var firstMoveCommpensation = false
     var imageHolder = [UIImage]()
     
     let image1 = UIImage(named: "blocktile.jpg")!
@@ -36,6 +37,18 @@ class GameplayController: UIViewController {
     @IBAction func moveDown(_ sender: Any) {
         
         if movement > 0{
+            //adjust the image forward by one
+            i += 1
+            //because of initial image configuration have to adjust the first move
+            if firstMoveCommpensation  == false{
+                i = 0
+                firstMoveCommpensation = true
+            }
+            //if i is pointing past the last image in the array reset back to begining
+            if i > 4 {
+                i = 0
+            }
+            //itterate through the tiles assigning the images to the tiles
             for tile in tiles!{
                 tile.image = imageHolder[i]
                 i += 1
@@ -43,6 +56,7 @@ class GameplayController: UIViewController {
                     i = 0
                 }
             }
+            //reset i back to its initial position + 1
             counter += 1
             if counter >= 5 {
                 counter = 0
@@ -58,18 +72,30 @@ class GameplayController: UIViewController {
     
     @IBAction func moveUp(_ sender: Any) {
         if movement > 0{
+            i -= 1
+            
+            if firstMoveCommpensation  == false{
+                i = 3
+                counter = 4
+                firstMoveCommpensation = true
+            }
+            if i < 0{
+                i = 4
+            }
             for tile in tiles!{
-                tile.image = imageHolder[4 - i]
-                i -= 1
-                if i < 0 {
-                    i = 4
+                tile.image = imageHolder[i]
+                i += 1
+                if i > 4 {
+                    i = 0
                 }
             }
-            i = counter
-            counter += 1
-            if counter > 4 {
-                counter = 0
+    
+            counter -= 1
+            
+            if counter < 0 {
+                counter = 4
             }
+            i = counter
             movement -= 1
             self.movementLabel.text = "Moves Left: \(movement)"
         }
